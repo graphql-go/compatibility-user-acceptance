d7544ac110f15f25b45ef31082fa1529b2030f0c
Chris Ramón <rchristian.ramon@gmail.com>
2025-04-08 13:27:15 -0500

    main: wires extractor component

--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/graphql-go/compatibility-base/config"
+	"github.com/graphql-go/compatibility-user-acceptance/extractor"
 )
 
 func main() {
 	cfg := config.Config{}
 
+	ex := extractor.Extractor{}
+	r, err := ex.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(cfg.IsDebug)
+	fmt.Printf("%+v\n", r)
 }
